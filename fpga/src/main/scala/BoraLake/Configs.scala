2f29277171ea572f72c82d2e73f94fb943d8bcdb
naichenzhao <naichenzhao13@gmail.com>
2025-05-22 17:58:09 -0700

    initial bora lake stuff

---
+++ fpga/src/main/scala/BoraLake/Configs.scala
@@ -0,0 +1,96 @@
+// See LICENSE for license details.
+package chipyard.fpga.boralake
+
+import org.chipsalliance.cde.config._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.devices.debug._
+import freechips.rocketchip.devices.tilelink._
+import org.chipsalliance.diplomacy._
+import org.chipsalliance.diplomacy.lazymodule._
+import freechips.rocketchip.system._
+import freechips.rocketchip.tile._
+
+import sifive.blocks.devices.uart._
+import sifive.fpgashells.shell.{DesignKey}
+
+import testchipip.serdes._
+
+import chipyard.{BuildSystem}
+import org.chipsalliance.cde.config.{Config}
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.subsystem.{MBUS, SBUS}
+import testchipip.soc.{OBUS}
+
+// don't use FPGAShell's DesignKey
+class WithNoDesignKey extends Config((site, here, up) => {
+  case DesignKey => (p: Parameters) => new SimpleLazyRawModule()(p)
+})
+
+// By default, this uses the on-board USB-UART for the TSI-over-UART link
+class WithBoraLakeTweaks(freqMHz: Double = 50) extends Config(
+  new WithBoraLakePMODUART ++
+  new WithBoraLakeUARTTSI ++
+  new WithBoraLakeJTAG ++
+  // new WithBoraLakeTDDRTL ++
+
+  new WithNoDesignKey ++
+  new testchipip.tsi.WithUARTTSIClient(initBaudRate = 921600) ++
+  new chipyard.harness.WithSerialTLTiedOff ++
+  new chipyard.harness.WithHarnessBinderClockFreqMHz(freqMHz) ++
+  new chipyard.config.WithUniformBusFrequencies(freqMHz) ++
+  new chipyard.harness.WithAllClocksFromHarnessClockInstantiator ++
+  new chipyard.clocking.WithPassthroughClockGenerator ++
+
+  // new chipyard.config.WithTLBackingMemory ++ // FPGA-shells converts the AXI to TL for us
+  // new freechips.rocketchip.subsystem.WithExtMemSize(BigInt(256) << 20) ++ // 256mb
+
+  new freechips.rocketchip.subsystem.WithNoMemPort ++ 
+  new testchipip.serdes.WithNoSerialTL ++
+  
+  new freechips.rocketchip.subsystem.WithoutTLMonitors)
+
+
+class BoraLakeRocketConfig extends Config(
+  new WithBoraLakeTweaks(freqMHz = 50) ++
+  new chipyard.config.WithBroadcastManager ++ // no l2
+  new chipyard.MediumBoomV4Config)
+
+
+
+
+// A simple config demonstrating a "bringup prototype" to bringup the ChipLikeRocketconfig
+class BoraLakeBringupHostConfig extends Config(
+  new WithBoraLakeSerialTLToGPIO ++
+  new WithBoraLakeTweaks(freqMHz = 50) ++
+  new chipyard.iobinders.WithOldSerialTLPunchthrough ++                // Don't generate IOCells for the serial TL (this design maps to FPGA)
+  //=============================
+  // Setup the SerialTL side on the bringup device
+  //=============================
+  new testchipip.serdes.old.WithSerialTL(Seq(
+    testchipip.serdes.old.SerialTLParams(
+      manager = Some(testchipip.serdes.old.SerialTLManagerParams(
+        memParams = Seq(
+          testchipip.serdes.old.ManagerRAMParams(                            // Bringup platform can access all memory from 0 to DRAM_BASE
+            address = BigInt("00000000", 16),
+            size    = BigInt("10070000", 16)),
+          testchipip.serdes.old.ManagerRAMParams(                            // Bringup platform can access all memory from 0 to DRAM_BASE
+            address = BigInt("14000000", 16),
+            size    = BigInt("6C000000", 16)),
+      ))),
+      client = Some(testchipip.serdes.old.SerialTLClientParams()),                                        // Allow chip to access this device's memory (DRAM)
+      phyParams = testchipip.serdes.old.InternalSyncSerialParams(width=8, freqMHz = 50)), // bringup platform provides the clock
+    
+    testchipip.serdes.old.SerialTLParams(
+      manager = None,
+      client = Some(testchipip.serdes.old.SerialTLClientParams()),                                        // Allow chip to access this device's memory (DRAM)
+      phyParams = testchipip.serdes.old.InternalSyncSerialParams(width=1, freqMHz = 50)), // bringup platform provides the clock   
+    )) ++
+  new testchipip.serdes.WithNoSerialTL ++
+
+  //============================
+  // Setup bus topology on the bringup system
+  //============================
+  new testchipip.soc.WithOffchipBusClient(SBUS,                                 // offchip bus hangs off the SBUS
+    blockRange = AddressSet.misaligned(0x100000000L, (BigInt(1) << 30) * 4)) ++ // offchip bus should not see the main memory of the testchip, since that can be accessed directly
+  new testchipip.soc.WithOffchipBus ++                                          // offchip bus
+  new chipyard.NoCoresConfig)
---
+++ fpga/src/main/scala/BoraLake/HarnessBinders.scala
@@ -0,0 +1,176 @@
+package chipyard.fpga.boralake
+
+import chisel3._
+
+import freechips.rocketchip.jtag.{JTAGIO}
+import freechips.rocketchip.subsystem.{PeripheryBusKey}
+import freechips.rocketchip.tilelink.{TLBundle}
+import freechips.rocketchip.diplomacy.{LazyRawModuleImp}
+import org.chipsalliance.diplomacy.nodes.{HeterogeneousBag}
+import sifive.blocks.devices.uart.{UARTPortIO, UARTParams}
+import sifive.blocks.devices.jtag.{JTAGPins, JTAGPinsFromPort}
+import sifive.blocks.devices.pinctrl.{BasePin}
+import sifive.fpgashells.shell._
+import sifive.fpgashells.ip.xilinx._
+import sifive.fpgashells.shell.xilinx._
+import sifive.fpgashells.clocks._
+import chipyard._
+import chipyard.harness._
+import chipyard.iobinders._
+import testchipip.serdes._
+import testchipip.serdes.old._
+
+class WithBoraLakeUARTTSI extends HarnessBinder({
+  case (th: HasHarnessInstantiators, port: UARTTSIPort, chipId: Int) => {
+    val ath = th.asInstanceOf[LazyRawModuleImp].wrapper.asInstanceOf[BoraLakeHarness]
+    val harnessIO = IO(new UARTPortIO(port.io.uartParams)).suggestName("uart_tsi")
+    harnessIO <> port.io.uart
+    val packagePinsWithPackageIOs = Seq(
+      ("A24" , IOPin(harnessIO.rxd)),
+      ("A23", IOPin(harnessIO.txd)))
+
+    packagePinsWithPackageIOs foreach { case (pin, io) => {
+      ath.xdc.addPackagePin(io, pin)
+      ath.xdc.addIOStandard(io, "LVCMOS33")
+      ath.xdc.addIOB(io)
+    } }
+  }
+})
+
+// class WithBoraLakeTDDRTL extends HarnessBinder({
+//   case (th: HasHarnessInstantiators, port: TLMemPort, chipId: Int) => {
+//     val ath = th.asInstanceOf[LazyRawModuleImp].wrapper.asInstanceOf[BoraLakeHarness]
+//     val bundles = ath.ddrClient.out.map(_._1)
+//     val ddrClientBundle = Wire(new HeterogeneousBag(bundles.map(_.cloneType)))
+//     bundles.zip(ddrClientBundle).foreach { case (bundle, io) => bundle <> io }
+//     ddrClientBundle <> port.io
+//   }
+// })
+
+class WithBoraLakeSerialTLToGPIO extends HarnessBinder({
+  case (th: HasHarnessInstantiators, port: OldSerialTLPort, chipId: Int) => {
+    val ath = th.asInstanceOf[LazyRawModuleImp].wrapper.asInstanceOf[BoraLakeHarness]
+    val harnessIO = IO(chiselTypeOf(port.io)).suggestName(s"serial_tl_old_${port.portId}")
+    harnessIO <> port.io
+
+    harnessIO match {
+      case io: testchipip.serdes.old.DecoupledSerialIO => {
+        val clkIO = io match {
+          case io: testchipip.serdes.old.InternalSyncSerialIO => IOPin(io.clock_out)
+          case io: testchipip.serdes.old.ExternalSyncSerialIO => IOPin(io.clock_in)
+        }
+
+        val packagePinsWithPackageIOs = if (port.portId == 0) {
+          Seq(
+            ("J17", clkIO),
+
+            ("D15", IOPin(io.in.valid)),
+            ("E16", IOPin(io.in.ready)),
+
+            ("C16", IOPin(io.in.bits, 0)),
+            ("A18", IOPin(io.in.bits, 1)),
+            ("F18", IOPin(io.in.bits, 2)),
+            ("H17", IOPin(io.in.bits, 3)),
+            ("B18", IOPin(io.in.bits, 4)),
+            ("E17", IOPin(io.in.bits, 5)),
+            ("A15", IOPin(io.in.bits, 6)),
+            ("B16", IOPin(io.in.bits, 7)),
+
+            ("J18", IOPin(io.out.valid)),
+            ("G17", IOPin(io.out.ready)),
+
+            ("E18", IOPin(io.out.bits, 0)),
+            ("C15", IOPin(io.out.bits, 1)),
+            ("D18", IOPin(io.out.bits, 2)),
+            ("G18", IOPin(io.out.bits, 3)),
+            ("C17", IOPin(io.out.bits, 4)),
+            ("D17", IOPin(io.out.bits, 5)),
+            ("C14", IOPin(io.out.bits, 6)),
+            ("B17", IOPin(io.out.bits, 7)),
+          )
+        } else {
+          Seq(
+            ("D12", clkIO),
+
+            ("B11", IOPin(io.out.valid)),
+            ("A13", IOPin(io.out.ready)),
+
+            ("B12", IOPin(io.out.bits, 0)),
+
+            ("A14", IOPin(io.in.valid)),
+            ("A11", IOPin(io.in.ready)),
+
+            ("C12", IOPin(io.in.bits, 0)),
+          )
+        }
+        packagePinsWithPackageIOs foreach { case (pin, io) => {
+          ath.xdc.addPackagePin(io, pin)
+          ath.xdc.addIOStandard(io, "LVCMOS12")
+        }}
+
+        // // Don't add IOB to the clock, if its an input
+        // io match {
+        //   case io: testchipip.serdes.old.InternalSyncSerialIO => packagePinsWithPackageIOs foreach { case (pin, io) => {
+        //     ath.xdc.addIOB(io)
+        //   }}
+        //   case io: testchipip.serdes.old.ExternalSyncSerialIO => packagePinsWithPackageIOs.drop(1).foreach { case (pin, io) => {
+        //     ath.xdc.addIOB(io)
+        //   }}
+        // }
+
+        ath.sdc.addClock("ser_tl_clock", clkIO, 100)
+        ath.sdc.addGroup(pins = Seq(clkIO))
+        ath.xdc.clockDedicatedRouteFalse(clkIO)
+      }
+    }
+  }
+})
+
+// Maps the UART device to the on-board USB-UART
+class WithBoraLakeUART(rxdPin: String = "N26", txdPin: String = "M26") extends HarnessBinder({
+  case (th: HasHarnessInstantiators, port: UARTPort, chipId: Int) => {
+    val ath = th.asInstanceOf[LazyRawModuleImp].wrapper.asInstanceOf[BoraLakeHarness]
+    val harnessIO = IO(chiselTypeOf(port.io)).suggestName("uart")
+    harnessIO <> port.io
+    val packagePinsWithPackageIOs = Seq(
+      (rxdPin, IOPin(harnessIO.rxd)),
+      (txdPin, IOPin(harnessIO.txd)))
+    packagePinsWithPackageIOs foreach { case (pin, io) => {
+      ath.xdc.addPackagePin(io, pin)
+      ath.xdc.addIOStandard(io, "LVCMOS33")
+      ath.xdc.addIOB(io)
+    } }
+  }
+})
+
+// Maps the UART device to PMOD JD pins 3/7
+class WithBoraLakePMODUART extends WithBoraLakeUART("R25", "P25")
+
+class WithBoraLakeJTAG extends HarnessBinder({
+  case (th: HasHarnessInstantiators, port: JTAGPort, chipId: Int) => {
+    val ath = th.asInstanceOf[LazyRawModuleImp].wrapper.asInstanceOf[BoraLakeHarness]
+    val harnessIO = IO(new JTAGChipIO(false)).suggestName("jtag")
+    harnessIO.TDO := port.io.TDO
+    port.io.TCK := harnessIO.TCK
+    port.io.TDI := harnessIO.TDI
+    port.io.TMS := harnessIO.TMS
+    port.io.reset.foreach(_ := th.referenceReset)
+
+    ath.sdc.addClock("JTCK", IOPin(harnessIO.TCK), 10)
+    ath.sdc.addGroup(clocks = Seq("JTCK"))
+    ath.xdc.clockDedicatedRouteFalse(IOPin(harnessIO.TCK))
+    val packagePinsWithPackageIOs = Seq(
+      ("K25", IOPin(harnessIO.TCK)),
+      ("K26", IOPin(harnessIO.TMS)),
+      ("R26", IOPin(harnessIO.TDI)),
+      ("P26", IOPin(harnessIO.TDO)),
+    )
+    packagePinsWithPackageIOs foreach { case (pin, io) => {
+      ath.xdc.addPackagePin(io, pin)
+      ath.xdc.addIOStandard(io, "LVCMOS33")
+      ath.xdc.addPullup(io)
+    } }
+  }
+})
+
+
